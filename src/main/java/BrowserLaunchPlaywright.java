1f627c7bf40360f3d613cf506e645682fdd3d03d
Navi <navitony5983@gmail.com>
2025-11-10 09:14:37 +0530

    Created Playwright project with tests

--- src/main/java/BrowserLaunchPlaywright.java
+++ src/main/java/BrowserLaunchPlaywright.java
@@ -1,25 +1,35 @@
 import com.microsoft.playwright.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BrowserLaunchPlaywright {
+    Playwright playwright;
+    Browser browser;
+    Page page;
+    @Before
+    public void lauch(){
+        playwright= Playwright.create();
+        BrowserType.LaunchOptions lp = new BrowserType.LaunchOptions();
+        lp.setChannel("chrome");
+        lp.setHeadless(false);
+        browser = playwright.chromium().launch(lp);
+        page = browser.newPage();
+    }
 
+    @Test
+    public void test(){
 
-
-        public static void main(String[] args) {
-            Playwright playwright = Playwright.create();
-            BrowserType.LaunchOptions lp = new BrowserType.LaunchOptions();
-            lp.setChannel("chrome");
-            lp.setHeadless(false);
-            Browser browser = playwright.chromium().launch(lp);
-            Page page = browser.newPage();
-            page.navigate("https://www.saucedemo.com/");
-            System.out.println(page.title());
-            page.fill("//*[@id='user-name']","standard_user");
-            page.fill("//*[@id='password']","secret_sauce");
-            page.click("//*[@id='login-button']");
-            System.out.println(page.title());
-            page.close();
-            browser.close();
-            playwright.close();
-
-        }
+        page.navigate("https://www.saucedemo.com/");
+        System.out.println(page.title());
+        page.fill("//*[@id='user-name']","standard_user");
+        page.fill("//*[@id='password']","secret_sauce");
+        page.click("//*[@id='login-button']");
+        System.out.println(page.title());
+        page.close();
+        browser.close();
+        playwright.close();
+    }
+    public static void main(String[] args) {
+    }
     }
 
